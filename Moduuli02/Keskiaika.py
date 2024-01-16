d516947a626555c2d2d0d9afc7153c3748d0e3b6
Patrik Skogberg <skogbergp@gmail.com>
2024-01-16 12:34:37 +0200

    new files

--- Moduuli02/Keskiaika.py
+++ Moduuli02/Keskiaika.py
@@ -1,3 +1,14 @@
 #leviskä = 20 naulaa
 #naula = 32 luotia
 #luot =13.3 grammaa
+leviskä = int(input("Anna leviskät: "))
+naulat = int(input("Anna naulat: "))
+luodit = int(input("Anna luodit: "))
+
+leviskä_grams = leviskä * 20 * 32 *13.3
+naulat_grams = naulat * 32 * 13.3
+luodit_grams = luodit * 13.3
+mass = leviskä_grams + naulat_grams + luodit_grams
+kilograms = mass // 1000
+grams = mass % 1000
+print(f"Massa nykymittojen mukaan:\n{int(kilograms)} kilogrammaa ja {grams:.2f} grammaa. ")
---
+++ Moduuli02/numeroLukko.py
@@ -0,0 +1,14 @@
+import random
+
+numero_1 = random.randint(1,9)
+numero_2 = random.randint(1,9)
+numero_3 = random.randint(1,9)
+
+print(numero_1,numero_2,numero_3)
+
+numero_1 = random.randint(1,6)
+numero_2 = random.randint(1,6)
+numero_3 = random.randint(1,6)
+numero_4 = random.randint(1,6)
+#korvaan aiemmat numerot
+print(numero_1,numero_2,numero_3)
