8303b42100c299cd5b6c43e61c9e33e465be0abe
Patrik Skogberg <skogbergp@gmail.com>
2024-01-23 10:54:32 +0200

    SuurinPienin.py added

---
+++ moduuli04/suurinPienin.py
@@ -0,0 +1,11 @@
+user_number = input("Syötä numero: ")
+big = small = float(user_number)
+while user_number != "":
+    user_number = float(user_number)
+    if user_number > big:
+        big = user_number
+    if user_number < small:
+        small = user_number
+    user_number = input("Syötä numero: ")
+print(f"Pienin luku: {small}")
+print(f"Suurin luku: {big}")
