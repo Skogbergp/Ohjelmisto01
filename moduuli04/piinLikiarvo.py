0d283d50b4b251eb206994ec19f6871a75f197ed
Patrik Skogberg <skogbergp@gmail.com>
2024-01-23 10:25:29 +0200

    piin likiarvon laskin

---
+++ moduuli04/piinLikiarvo.py
@@ -0,0 +1,19 @@
+""""
+Ympyrä A
+neliö B
+Pisteiden kokonaismäärä N
+pisteet ympyrän sisällä n
+"""
+
+import random
+
+points_total = int(input("Syötä pisteiden kokonaismäärä: "))
+points_in_circle = 0
+i = x = y = 0
+while i < points_total:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if (x ** 2) + (y ** 2) < 1:
+        points_in_circle += 1
+    i += 1
+print(f"piin likiarvo: {(4 * points_in_circle) / points_total}")
